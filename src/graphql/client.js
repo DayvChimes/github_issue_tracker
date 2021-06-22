310b1d6e6af5cdc3e51ce4d4a67307bbb39bf34a
David Njagah <dnjagah@gmail.com>
2021-06-23 00:03:33 +0300

    Worked on Async Apollo graphql actions

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -12,7 +12,7 @@ const link = new HttpLink({
   uri: "https://api.github.com/graphql",
 });
 
-const token = "place_token_here";
+const token = "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"
 
 const authMiddleware = new ApolloLink((operation, forward) => {
   // add the authorization to the headers
